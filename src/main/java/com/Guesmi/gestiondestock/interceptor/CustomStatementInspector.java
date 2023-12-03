d715c772d24883f6f623abf2408bee23c1db8d03
Guesmi Achref <guesmi.achraf3@gmail.com>
2023-12-03 12:58:29 +0100

    adding Hibernate interceptor

--- src/main/java/com/Guesmi/gestiondestock/interceptor/CustomStatementInspector.java
+++ src/main/java/com/Guesmi/gestiondestock/interceptor/CustomStatementInspector.java
@@ -1,21 +1,31 @@
 package com.Guesmi.gestiondestock.interceptor;
 
 import org.hibernate.resource.jdbc.spi.StatementInspector;
+import org.slf4j.MDC;
 import org.springframework.util.StringUtils;
 
-/*
+
 public class CustomStatementInspector implements StatementInspector {
 
     @Override
     public String inspect(String sql) {
         if (StringUtils.hasLength(sql) && sql.toLowerCase().startsWith("select")) {
-            if (sql.contains("where")) {
-                sql = sql + " and idEntreprise = 1";
-            } else {
-                sql = sql + " where idEntreprise = 1";
+            // select utilisateu0_.
+            final String entityName = sql.substring(7, sql.indexOf("."));
+            final String idEntreprise = MDC.get("idEntreprise");
+            if (StringUtils.hasLength(entityName)
+                    && !entityName.toLowerCase().contains("entreprise")
+                    && !entityName.toLowerCase().contains("roles")
+                    && StringUtils.hasLength(idEntreprise)) {
+
+                if (sql.contains("where")) {
+                    sql = sql + " and " + entityName + ".identreprise = " + idEntreprise;
+                } else {
+                    sql = sql + " where " + entityName + ".identreprise = " + idEntreprise;
+                }
             }
         }
         return sql;
     }
 }
-*/
+
